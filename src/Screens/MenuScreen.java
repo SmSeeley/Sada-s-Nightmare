26e65dcfa2cb65cdc38e6a796fe52d5c14117488
SmSeeley <smseeley@quinnipiac.edu>
2025-10-05 14:12:03 -0400

    added a new title screen to our game

--- src/Screens/MenuScreen.java
+++ src/Screens/MenuScreen.java
@@ -1,25 +1,37 @@
 package Screens;
 
-import Engine.*;
+import Engine.GraphicsHandler;
+import Engine.ImageLoader;
+import Engine.Key;
+import Engine.KeyLocker;
+import Engine.Keyboard;
+import Engine.Screen;
 import Game.GameState;
 import Game.ScreenCoordinator;
-import Level.Map;
-import Maps.TitleScreenMap;
 import SpriteFont.SpriteFont;
 
-import java.awt.*;
+import java.awt.Color;
+import java.awt.image.BufferedImage;
 
-// This is the class for the main menu screen
 public class MenuScreen extends Screen {
-    protected ScreenCoordinator screenCoordinator;
-    protected int currentMenuItemHovered = 0; // current menu item being "hovered" over
-    protected int menuItemSelected = -1;
-    protected SpriteFont playGame;
-    protected SpriteFont credits;
-    protected Map background;
-    protected int keyPressTimer;
-    protected int pointerLocationX, pointerLocationY;
-    protected KeyLocker keyLocker = new KeyLocker();
+    private final ScreenCoordinator screenCoordinator;
+
+    private SpriteFont playGameText;
+    private SpriteFont creditsText;
+
+    private final KeyLocker keyLocker = new KeyLocker();
+    private int hoveredIndex = 0;       
+    private int keyRepeatTimer = 0;
+
+    private BufferedImage titleBg;
+
+    private static final int GAME_W = 800;   //window width
+    private static final int GAME_H = 600;   //window height
+    private static final int IMAGE_Y_OFFSET = 40; 
+
+    // Menu text locations
+    private static final int PLAY_Y    = 240; // "PLAY GAME" Y
+    private static final int CREDITS_Y = 290; // "CREDITS"  Y
 
     public MenuScreen(ScreenCoordinator screenCoordinator) {
         this.screenCoordinator = screenCoordinator;
@@ -27,74 +39,88 @@ public class MenuScreen extends Screen {
 
     @Override
     public void initialize() {
-        playGame = new SpriteFont("PLAY GAME", 200, 123, "Arial", 30, new Color(49, 207, 240));
-        playGame.setOutlineColor(Color.black);
-        playGame.setOutlineThickness(3);
-        credits = new SpriteFont("CREDITS", 200, 223, "Arial", 30, new Color(49, 207, 240));
-        credits.setOutlineColor(Color.black);
-        credits.setOutlineThickness(3);
-        background = new TitleScreenMap();
-        background.setAdjustCamera(false);
-        keyPressTimer = 0;
-        menuItemSelected = -1;
+        
+        titleBg = ImageLoader.load("sada/Title_Screen.png");
+
+        // Center text 
+        String playStr = "PLAY GAME";
+        String creditsStr = "CREDITS";
+
+        int playWidth = playStr.length() * 14;
+        int creditsWidth = creditsStr.length() * 14;
+
+        int playX = (GAME_W - playWidth) / 2 - 20;
+        int creditsX = (GAME_W - creditsWidth) / 2 - 20;
+
+        playGameText = new SpriteFont(playStr, playX, PLAY_Y, "Comic Sans MS", 28, new Color(80,160,255));
+        playGameText.setOutlineColor(Color.BLACK);
+        playGameText.setOutlineThickness(3);
+
+        creditsText  = new SpriteFont(creditsStr, creditsX, CREDITS_Y, "Comic Sans MS", 28, new Color(80,160,255));
+        creditsText.setOutlineColor(Color.BLACK);
+        creditsText.setOutlineThickness(3);
+
         keyLocker.lockKey(Key.SPACE);
+        keyLocker.lockKey(Key.ENTER);
     }
 
+    @Override
     public void update() {
-        // update background map (to play tile animations)
-        background.update(null);
 
-        // if down or up is pressed, change menu item "hovered" over (blue square in front of text will move along with currentMenuItemHovered changing)
-        if (Keyboard.isKeyDown(Key.DOWN) && keyPressTimer == 0) {
-            keyPressTimer = 14;
-            currentMenuItemHovered++;
-        } else if (Keyboard.isKeyDown(Key.UP) && keyPressTimer == 0) {
-            keyPressTimer = 14;
-            currentMenuItemHovered--;
-        } else {
-            if (keyPressTimer > 0) {
-                keyPressTimer--;
-            }
+        if (keyRepeatTimer > 0) keyRepeatTimer--;
+
+        if (Keyboard.isKeyDown(Key.DOWN) && keyRepeatTimer == 0) {
+            hoveredIndex = (hoveredIndex + 1) % 2;
+            keyRepeatTimer = 14;
+        }
+        if (Keyboard.isKeyDown(Key.UP) && keyRepeatTimer == 0) {
+            hoveredIndex = (hoveredIndex + 1) % 2;
+            keyRepeatTimer = 14;
         }
 
-        // if down is pressed on last menu item or up is pressed on first menu item, "loop" the selection back around to the beginning/end
-        if (currentMenuItemHovered > 1) {
-            currentMenuItemHovered = 0;
-        } else if (currentMenuItemHovered < 0) {
-            currentMenuItemHovered = 1;
+        // Select with SPACE or ENTER
+        boolean selectPressed =
+                (Keyboard.isKeyDown(Key.SPACE) && !keyLocker.isKeyLocked(Key.SPACE)) ||
+                (Keyboard.isKeyDown(Key.ENTER) && !keyLocker.isKeyLocked(Key.ENTER));
+
+        if (selectPressed) {
+            if (hoveredIndex == 0) screenCoordinator.setGameState(GameState.LEVEL);
+            else                    screenCoordinator.setGameState(GameState.CREDITS);
         }
 
-        // sets location for blue square in front of text (pointerLocation) and also sets color of spritefont text based on which menu item is being hovered
-        if (currentMenuItemHovered == 0) {
-            playGame.setColor(new Color(255, 215, 0));
-            credits.setColor(new Color(49, 207, 240));
-            pointerLocationX = 170;
-            pointerLocationY = 130;
-        } else if (currentMenuItemHovered == 1) {
-            playGame.setColor(new Color(49, 207, 240));
-            credits.setColor(new Color(255, 215, 0));
-            pointerLocationX = 170;
-            pointerLocationY = 230;
-        }
-
-        // if space is pressed on menu item, change to appropriate screen based on which menu item was chosen
-        if (Keyboard.isKeyUp(Key.SPACE)) {
-            keyLocker.unlockKey(Key.SPACE);
-        }
-        if (!keyLocker.isKeyLocked(Key.SPACE) && Keyboard.isKeyDown(Key.SPACE)) {
-            menuItemSelected = currentMenuItemHovered;
-            if (menuItemSelected == 0) {
-                screenCoordinator.setGameState(GameState.LEVEL);
-            } else if (menuItemSelected == 1) {
-                screenCoordinator.setGameState(GameState.CREDITS);
-            }
-        }
+        if (!Keyboard.isKeyDown(Key.SPACE)) keyLocker.unlockKey(Key.SPACE);
+        if (!Keyboard.isKeyDown(Key.ENTER)) keyLocker.unlockKey(Key.ENTER);
     }
 
-    public void draw(GraphicsHandler graphicsHandler) {
-        background.draw(graphicsHandler);
-        playGame.draw(graphicsHandler);
-        credits.draw(graphicsHandler);
-        graphicsHandler.drawFilledRectangleWithBorder(pointerLocationX, pointerLocationY, 20, 20, new Color(49, 207, 240), Color.black, 2);
+    @Override
+    public void draw(GraphicsHandler g) {
+        
+        if (titleBg != null) {
+            double scale = Math.max(
+                (double) GAME_W / titleBg.getWidth(),
+                (double) GAME_H / titleBg.getHeight()
+            );
+            int drawW = (int) (titleBg.getWidth() * scale);
+            int drawH = (int) (titleBg.getHeight() * scale);
+            int drawX = (GAME_W - drawW) / 2;
+            int drawY = (GAME_H - drawH) / 2 - IMAGE_Y_OFFSET; // raise image
+
+            g.drawImage(titleBg, drawX, drawY, drawW, drawH);
+        } else {
+            g.drawFilledRectangle(0, 0, GAME_W, GAME_H, Color.BLACK);
+        }
+
+        // Hover highlight
+        if (hoveredIndex == 0) {
+            playGameText.setColor(new Color(240,200,80));  // gold
+            creditsText.setColor(new Color(80,160,255));
+        } else {
+            playGameText.setColor(new Color(80,160,255));
+            creditsText.setColor(new Color(240,200,80));
+        }
+
+        // Draw menu options (manually centered)
+        playGameText.draw(g);
+        creditsText.draw(g);
     }
 }
