0fdec1f821bdacfa42c4f582711e0f1406d11954
SmSeeley <smseeley@quinnipiac.edu>
2025-10-19 23:44:49 -0400

    added decorations to rooms

--- src/Maps/FirstRoom.java
+++ src/Maps/FirstRoom.java
@@ -3,7 +3,6 @@ package Maps;
 
 import java.util.ArrayList;
 import EnhancedMapTiles.*;
-import EnhancedMapTiles.Door;
 import Level.*;
 import Tilesets.CommonTileset;
 import Tilesets.DungeonTileset;
@@ -23,6 +22,33 @@ public class FirstRoom extends Map {
     @Override
     public ArrayList<EnhancedMapTile> loadEnhancedMapTiles() {
         ArrayList<EnhancedMapTile> enhancedMapTiles = new ArrayList<>();
+
+
+        WaterBarrel WaterBarrel_2 = new WaterBarrel(getMapTile(4, 9).getLocation());
+        enhancedMapTiles.add(WaterBarrel_2);
+
+        Skull Skull_1 = new Skull(getMapTile(12, 4).getLocation());
+        enhancedMapTiles.add(Skull_1);
+
+        Skull Skull_2 = new Skull(getMapTile(11, 9).getLocation());
+        enhancedMapTiles.add(Skull_2);
+
+        Blood Blood_1 = new Blood(getMapTile(6, 8).getLocation());
+        enhancedMapTiles.add(Blood_1);
+
+        Blood Blood_2 = new Blood(getMapTile(9, 4).getLocation());
+        enhancedMapTiles.add(Blood_2);
+
+        Blood Blood_3 = new Blood(getMapTile(12, 7).getLocation());
+        enhancedMapTiles.add(Blood_3);
+
+        Emptybarrel EmptyBarrel_1 = new Emptybarrel(getMapTile(3, 4).getLocation());
+        enhancedMapTiles.add(EmptyBarrel_1);
+
+        Emptybarrel EmptyBarrel_2 = new Emptybarrel(getMapTile(4, 6).getLocation());
+        enhancedMapTiles.add(EmptyBarrel_2);
+
+        
 
         Door toSecond = new Door(getMapTile(8, 2).getLocation())
         .toMap("SecondRoom", 10, 12)         // spawn tile (x=21, y=3) in SecondRoom
